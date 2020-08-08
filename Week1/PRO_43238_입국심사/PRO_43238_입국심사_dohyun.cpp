3d98034000f5cba22fa498ec5c4ab16423cff4f6
dohyun1411 <dohyun141@kaist.ac.kr>
2020-08-09 05:29:46 +0900

    Add comments to PRO_43238_입국심사_dohyun.cpp

--- Week1/PRO_43238_입국심사/PRO_43238_입국심사_dohyun.cpp
+++ Week1/PRO_43238_입국심사/PRO_43238_입국심사_dohyun.cpp
@@ -1,34 +1,30 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
 unsigned long long solution(int n, vector<int> times) {
-    sort(times.begin(), times.end());
-    unsigned long long i = 0, j = times[times.size()-1]*n;
+    /*
+    이진탐색 이용
+    주어진 시간(mid) 내에 심사받을 수 있는 사람 수를 구한 후,
+    그 값을 n과 비교하여 더 작으면 시간을 늘리고(i=mid+1) 더 크면 시간을 줄여가며(j=mid-1)
+    적절한 시간을 찾는다.    
+    */
+    sort(times.begin(), times.end()); // times 오름차순 정렬
+    unsigned long long i = 0, j = times[times.size()-1]*n; // i는 시간의 최솟값, j는 최댓값
     unsigned long long answer = j;
-    while (i<=j) {
-        unsigned long long mid = (i+j)/2;
-        unsigned long long sum = 0;
+    while (i<=j) { // 이분탐색 실행
+        unsigned long long mid = (i+j)/2; // 중간값
+        unsigned long long sum = 0; // 심사받을 수 있는 사람 수(가 될 예정)
         for (int time: times) {
             sum += mid/time;
         }
-        if (sum>=n) {
-            j = mid-1;
+        if (sum>=n) { // 심사받을 수 있는 사람 수가 n보다 크면,
+            j = mid-1; // 시간을 줄인다.
             answer = min(answer, mid);
         }
-        else {
-            i = mid+1;
+        else { // 그렇지 않으면,
+            i = mid+1; // 시간을 늘린다.
         }
     }
     return answer;
-}
-
-int main() {
-    int n = 6;
-    vector<int> times;
-    times.push_back(7);
-    times.push_back(10);
-    cout << solution(n, times) << endl;
-    return 0;
 }
